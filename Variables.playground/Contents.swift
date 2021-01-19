d8bac159e76b429455b109b5d7f23a953d3930fe
faisal <faisal25march99@gmail.com>
2021-01-19 21:46:05 +0530

    let and var in swift

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Person
+var name = "Bob" // String
+
+var age = 51 // Integer
+var weight = 50.3 // Double
+var isOraganDoner = false // Boolean
+
+print(weight)
+
+weight = 20.74 // Variables are mutuable
+print(weight)
+
+// Constant ---> unmutuable
+
+let eyeColor = "Blue"
+
+
+
